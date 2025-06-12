e2cd198f7f07cef56e32bb4d7cff4715d551753f
Breno Rosa <71451784+Akunozz@users.noreply.github.com>
2025-06-12 10:54:40 -0300

    login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
@@ -24,10 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${manrope.variable} antialiased`}>
+          {children}
+        <Toaster position="top-center" richColors theme="light" />
       </body>
     </html>
   );
